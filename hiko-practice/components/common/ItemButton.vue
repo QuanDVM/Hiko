1cdb923f0adf01fe348c978aefe6e1829425b70d
QuanDVM <quandvm1995@users.noreply.github.com>
2021-01-17 10:15:15 +0700

    Comit coding

---
+++ hiko-practice/components/common/ItemButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="buttonGame"><slot></slot></button>
+</template>
+
+<style scoped>
+.buttonGame {
+  width: 170px;
+  height: 30px;
+  background: url("../../static/images/frame.png");
+  background-size: 100%;
+  background-repeat: no-repeat;
+  outline: none;
+  border: none;
+  font-size: 16px;
+  color: #FFFDF1;
+  margin-top: 26px;
+}
+</style>
---
+++ hiko-practice/components/items/ItemScroll.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="scroll" :style="styleScroll">
+    <div v-if="postionText === 'top'">
+      <p class="scroll__text">{{text}}</p>
+      <img src="../../static/images/scrollBottom.png" alt="logo" class="scroll__icon">
+    </div>
+    <div v-if="postionText === 'bottom'">
+      <img src="../../static/images/scrollTop.png" alt="logo" class="scroll__icon">
+      <p class="scroll__text">{{text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    styleScroll: {
+      type: Object,
+      required: false
+    },
+    text: {
+      type: String,
+      required: true,
+      default: 'text'
+    },
+    postionText: {
+      type: String,
+      required: true,
+      default: 'top'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.scroll {
+  width: 85px;
+  text-align: center;
+  color: #FFFFFF;
+
+  &__text {
+    font-size: 30px;
+    text-align: center;
+  }
+
+  &__icon {
+    width: 52px;
+    height: 40px;
+  }
+}
+
+@media only screen and (max-width: 480px) and (min-width: 320px){
+  .scroll {
+    font-size: 12px;
+  }
+}
+</style>
